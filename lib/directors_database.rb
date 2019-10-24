ebb255d36d85b047aa0239d1523f541fb22a2e4b
eunice kim <oonis86@gmail.com>
2019-10-24 19:12:17 +0000

    Done.

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -1,26 +1,11 @@
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  hash = {
-  "1st Director's Name"=>1234567890,
-  "2nd Director's Name"=>1234577890,
-  "3rd Director's Name"=>1234709136,
-  ...
-}
+
+require 'directors_database'
+require 'pp'
+
+def pretty_print_nds(nds)
+  pp nds
 end
+
