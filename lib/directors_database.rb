6610391fca72decb9d4499b135f650ccf3b67a22
eunice kim <oonis86@gmail.com>
2019-10-24 18:51:58 +0000

    Done.

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -1,13 +1,26 @@
-require 'yaml'
+require 'directors_database'
 
-def directors_database
-	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
-  @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
-end
-
-hash = {
+def directors_totals(nds)
+  # Remember, it's always OK to pretty print what you get *in* to make sure
+  # that you know what you're starting with!
+  #
+  #
+  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+  result = {
+  }
+  #
+  # Use loops, variables and the accessing method, [], to loop through the NDS
+  # and total up all the
+  # ...
+  # ...
+  # ...
+  #
+  #
+  # Be sure to return the result at the end!
+  hash = {
   "1st Director's Name"=>1234567890,
   "2nd Director's Name"=>1234577890,
   "3rd Director's Name"=>1234709136,
   ...
 }
+end
